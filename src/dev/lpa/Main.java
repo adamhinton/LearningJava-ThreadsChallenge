80e837227d2b2489804b8b554a4707c8dbe04a6d
adamd <adam.d.hinton@gmail.com>
2024-04-27 10:49:47 -0400

    almost solution

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package dev.lpa;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Thread threadWConstructorEven = new Thread(() ->{
+           for (int i=2; i<=10; i+=2){
+               System.out.println(i);
+           }
+        });
+
+
+        Thread threadWRunnableOdds = new Thread(new myRunnable());
+
+        threadWConstructorEven.start();
+        threadWRunnableOdds.start();
+
+    }
+
+    private static class myRunnable implements Runnable{
+
+        @Override
+        public void run() {
+            for (int i=1; i<=9; i+=2){
+                System.out.println(i);
+            }
         }
     }
 }
